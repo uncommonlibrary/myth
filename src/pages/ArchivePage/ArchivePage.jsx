bc340b9a5bfbd1ce4f4f3588f025cf7ca6544dec
uncommonlibrary <carmen916@gmail.com>
2024-08-14 23:01:12 +0800

    added archive page

--- src/pages/ArchivePage/ArchivePage.jsx
+++ src/pages/ArchivePage/ArchivePage.jsx
@@ -1,13 +1,72 @@
 import Navbar from "../../components/Navbar/Navbar";
 import SearchBar from "../../components/SearchBar/SearchBar";
-// import { useState, useEffect } from "react";
+import { useState, useEffect } from "react";
+import { deleteFromTBR } from "../../services/tbrService";
+
+const urlArchive = "https://api.airtable.com/v0/app80K0OB0akZ36aN/Table%201";
+const urlKeyArchive = `${import.meta.env.VITE_APIKEY}`;
 
 export default function ArchivePage() {
+  const [books, setBooks] = useState([]);
+  const [bookStatus, setBookStatus] = useState({});
+
+  useEffect(() => {
+    const fetchBooks = async () => {
+      try {
+        const response = await fetch(
+          `${urlArchive}?fields%5B%5D=title&fields%5B%5D=author&filterByFormula=location%3D%22archive%22&sort%5B0%5D%5Bfield%5D=title&sort%5B0%5D%5Bdirection%5D=asc`,
+          {
+            method: "GET",
+            headers: {
+              Authorization: `Bearer ${urlKeyArchive}`,
+            },
+          }
+        );
+
+        if (!response.ok) {
+          throw new Error(`Response status: ${response.status}`);
+        }
+
+        const json = await response.json();
+        const records = json.records;
+        setBooks(records);
+        console.log("Shelf:", records);
+      } catch (error) {
+        console.error(error.message);
+      }
+    };
+    fetchBooks();
+  }, []);
+
+  const handleDelete = async (recordID) => {
+    setBookStatus((prevStatus) => ({
+      ...prevStatus,
+      [recordID]: { ...prevStatus[recordID], isDeleted: true },
+    }));
+    await deleteFromTBR(recordID); //this deletes from Shelves Airtable
+    setBooks(books.filter((book) => book.id !== recordID));
+  };
+
   return (
     <>
       <Navbar />
       <SearchBar />
       <h1>Your Archive</h1>
+      {books.length > 0 ? (
+        <>
+          {books.map((book, index) => (
+            <div key={index}>
+              <h2>Title: {book.fields.title}</h2>
+              <h3>Author: {book.fields.author}</h3>
+              <button onClick={() => handleDelete(book.id)}>
+                {bookStatus[book.id]?.isDeleted ? "Deleted!" : "Delete"}
+              </button>
+            </div>
+          ))}
+        </>
+      ) : (
+        <p>Nothing here!</p>
+      )}
     </>
   );
 }
