4ef344d37eedb315fabc6b04b769a82b89be2cd5
uncommonlibrary <carmen916@gmail.com>
2024-08-15 14:41:15 +0800

    author and title displays on details

--- src/pages/BookDetailsPage/BookDetailsPage.jsx
+++ src/pages/BookDetailsPage/BookDetailsPage.jsx
@@ -1,11 +1,15 @@
 import Navbar from "../../components/Navbar/Navbar";
 import SearchBar from "../../components/SearchBar/SearchBar";
 import { useState, useEffect } from "react";
-import { getDescription } from "../../services/bookDetailsService";
+import {
+  getDescription,
+  getDataWithEdKey,
+} from "../../services/bookDetailsService";
 import { useLocation } from "react-router-dom";
 
 export default function BookDetailsPage() {
   const [bookDetails, setBookDetails] = useState(null);
+  const [result, setResult] = useState({});
   const location = useLocation();
   const editionKey = location.state?.editionKey;
   console.log("EditionKey:", editionKey);
@@ -14,6 +18,21 @@ export default function BookDetailsPage() {
   const decodeUrl = decodeURIComponent(currentUrl); //must decode url to get the book ID: https://www.geeksforgeeks.org/how-to-encode-and-decode-a-url-in-javascript/
   const bookId = decodeUrl.split("/").pop(); //splits string into array of substring and pops the last element which contains ID i need
   console.log("Book ID:", bookId);
+
+  //to fetch book title, author, and other details (different from book description)
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await getDataWithEdKey(editionKey);
+        setResult(data);
+      } catch (error) {
+        console.error(error.message);
+        return "No book found";
+      }
+    };
+
+    fetchData();
+  }, [editionKey]);
 
   //this is to display book description
   const getBookDetails = async (bookId) => {
@@ -31,8 +50,8 @@ export default function BookDetailsPage() {
       <Navbar />
       <SearchBar />
       <h1>Book Details</h1>
-      <h2>Title</h2>
-      <h3>Author</h3>
+      <h2>Title: {result.docs[0].title}</h2>
+      <h3>Author: {result.docs[0].author_name}</h3>
       <p>
         {bookDetails?.description?.value ??
           bookDetails?.description ??
--- src/services/bookDetailsService.js
+++ src/services/bookDetailsService.js
@@ -1,9 +1,10 @@
-const BASE_URL = "https://openlibrary.org/works/";
+const WORKS_URL = "https://openlibrary.org/works/";
+const SEARCH_URL = "https://openlibrary.org/search.json?q=";
 
 //bookID here will become my key
 export async function getDescription(bookId) {
   try {
-    const response = await fetch(`${BASE_URL}${bookId}.json`);
+    const response = await fetch(`${WORKS_URL}${bookId}.json`);
     if (!response.ok) {
       throw new Error(`Response status: ${response.status}`);
     }
@@ -13,5 +14,20 @@ export async function getDescription(bookId) {
   } catch (error) {
     console.error(error.message);
     return "No description available";
+  }
+}
+
+export async function getDataWithEdKey(editionKey) {
+  try {
+    const response = await fetch(`${SEARCH_URL}${editionKey}`);
+    if (!response.ok) {
+      throw new Error(`Response status: ${response.status}`);
+    }
+
+    const json = await response.json();
+    return json; //need all the other deets: title author published ratings etc
+  } catch (error) {
+    console.error(error.message);
+    return "Data not available";
   }
 }
