7bc122fc1b2094794e1bc10839780e945ccba59a
uncommonlibrary <carmen916@gmail.com>
2024-08-16 10:10:56 +0800

    vercel image test upload

--- src/pages/MainPage/MainPage.jsx
+++ src/pages/MainPage/MainPage.jsx
@@ -19,7 +19,7 @@ export default function MainPage() {
       <div className="features">
         <div id="feature1">
           <img
-            src="https://www.dropbox.com/scl/fi/ee87102852eux1qfr80gj/trackreading.png?rlkey=jhizi4mzpb5y5917cxxqsu3zz&st=idf5wf37&dl=0"
+            src="https://www.dropbox.com/scl/fi/ee87102852eux1qfr80gj/trackreading.png?rlkey=jhizi4mzpb5y5917cxxqsu3zz&st=4o7s3pik&dl=0"
             alt="A girl reading"
           />
           <h2>Track your reading</h2>
@@ -30,7 +30,7 @@ export default function MainPage() {
         </div>
         <div id="feature2">
           <img
-            src="https://www.dropbox.com/scl/fi/wwg6e4soqyfrn39u9yto3/write_notes.png?rlkey=mh7cbhlrt1sl9eyu90z3lw0pg&st=9x71swwm&dl=0"
+            src="https://www.dropbox.com/scl/fi/wwg6e4soqyfrn39u9yto3/write_notes.png?rlkey=mh7cbhlrt1sl9eyu90z3lw0pg&st=q9w9gsum&dl=0"
             alt="A notebook"
           />
           <h2>Write notes</h2>
@@ -41,7 +41,7 @@ export default function MainPage() {
         </div>
         <div id="feature3">
           <img
-            src="https://www.dropbox.com/scl/fi/4t3th95velshjieofihtm/sharelibrary.png?rlkey=sv4046lh5g29jsxcjjn8solxh&st=2jgpmayf&dl=0"
+            src="https://www.dropbox.com/scl/fi/4t3th95velshjieofihtm/sharelibrary.png?rlkey=sv4046lh5g29jsxcjjn8solxh&st=v4zzxxxb&dl=0"
             alt="Two characters trading books"
           />
           <h2>Share your library</h2>
